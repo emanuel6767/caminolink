6fb5d6e83ca74a1e779148b136f8b206c15ddb10
emanuel6767 <67439557+emanuel6767@users.noreply.github.com>
2020-07-07 22:11:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header("Status: 301 Moved Permanently");
-header("Location:http://directounomasterles.blogspot.com/");
+header("Location:http://morenotamuyhot.blogspot.com/");
 
 //header("Location: https://www.google.com/");
 
